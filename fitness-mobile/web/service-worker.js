3b6ab84a2234275dd64dbcccc7c2cedd6274072c
enesabdullahi14-maker <enesabdullahi14@gmail.com>
2026-02-21 20:11:20 +0100

    Add files via upload

---
+++ fitness-mobile/web/service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'fitness-app-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        return response;
+      }).catch(() => caches.match('/index.html'));
+    })
+  );
+});
